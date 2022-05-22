2422609c690aec51da3ed9b49d78fc34d74f2d6c
Marcos Paulo <markstm10@gmail.com>
2022-05-21 22:28:18 -0300

    feat: creates Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,28 @@
 /* Incio dos imports*/
 import Head from "next/head";
+import Header from "../components/Header";
+
 import Menu from "../components/Menu";
 /* Fim dos imports*/
-function Home(){
-    return (
+function Home() {
+  return (
     <div>
-       <Head>
-       <meta charSet="utf-8" />
-            <meta name="robots" content="index, follow" />
-            <meta name="description" content="Plataforma para conectar profissional com clinetes de TI" />
-            <meta name="author" content="Rayson - Mendes" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <title>Santarém TI</title>            
-       </Head>
-       <Menu />
-       <div> 
-           
-        </div>    
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="Plataforma para conectar profissional com clinetes de TI"
+        />
+        <meta name="author" content="Rayson - Mendes" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Santarém TI</title>
+      </Head>
+      <Header />
+      <Menu />
+      <div></div>
     </div>
-    );
+  );
 }
 
 export default Home;
