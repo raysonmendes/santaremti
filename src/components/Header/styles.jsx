35d6b1909487fe97d18db27f60e74bb0048e5dc7
Marcos Paulo <markstm10@gmail.com>
2022-05-28 01:57:03 -0300

    fix: Header types errors

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  Header 'display: block;
+  display: block;
   background: #fafafa;
   position: absolute;
   width: 100%;
   height: 60px;
   left: 0px;
   top: 0px;
-  box-shadow: 0px 2px 10px rgba(34, 52, 89, 0.22);'
-  
+  box-shadow: 0px 2px 10px rgba(34, 52, 89, 0.22);
 `;
