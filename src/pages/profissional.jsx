6635069f3d04a9f25f6012328fb871c16b676379
Marcos Paulo <markstm10@gmail.com>
2022-05-28 02:00:32 -0300

    feat: Head - sets description prop on component

--- src/pages/profissional.jsx
+++ src/pages/profissional.jsx
@@ -3,10 +3,13 @@ import Head from "../components/Head";
 function Profissional() {
   return (
     <div>
-      <Head title="Profissional - Santarém TI" />
+      <Head
+        title="Profissional - Santarém TI"
+        description="Pagina de profissionais"
+      />
 
       <main>
-        {/*<Typogaphy>E Aqui Vai Um conteúdo de texto !</Typogaphy> */}
+        {/* <Typogaphy>E Aqui Vai Um conteúdo de texto !</Typogaphy> */}
       </main>
       {/* <Footer /> */}
     </div>
