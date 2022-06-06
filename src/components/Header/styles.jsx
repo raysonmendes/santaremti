91962e94f4259ac7f0a881bf0027ee48b77a0bdb
Rayson N Mendes <raysonmendes2.0@gmail.com>
2022-06-06 11:08:38 -0300

    redactor: Logo style sheet abstraction

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
-  display: block;
-  background: #fafafa;
+export const StyledHeader = styled.header`
+  display: flex;
   position: absolute;
-  width: 100%;
+  width: 1920px;
   height: 60px;
   left: 0px;
   top: 0px;
+  background: #fafafa;
   box-shadow: 0px 2px 10px rgba(34, 52, 89, 0.22);
 `;
