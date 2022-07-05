63d3c055a182f83ae6cd118c4be0632808c40f35
Rayson N Mendes <raysonmendes2.0@gmail.com>
2022-07-05 16:35:56 -0300

    update version tools

--- src/pages/api/auth/[...nextauth].jsx
+++ src/pages/api/auth/[...nextauth].jsx
@@ -1,23 +1,39 @@
-import NextAuth from "next-auth";
-import AppleProvider from "next-auth/providers/apple";
-import LinkedInProvider from "next-auth/providers/linkedin";
-import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
+// import NextAuth, { NextAuthOpitionsc } from "next-auth";
+// import LinkedInProvider from "next-auth/providers/linkedin";
+// import GoogleProvider from "next-auth/providers/google";
+// import EmailProvider from "next-auth/providers/email";
 
-export default NextAuth({
-  providers: [
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     LinkedInProvider({
+//       clientId: process.env.LINKEDIN_CLIENT_ID,
+//       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
 
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: "NextAuth.js <no-reply@example.com>",
-    }),
-  ],
-});
+//     EmailProvider({
+//       server: process.env.MAIL_SERVER,
+//       from: "NextAuth.js <no-reply@example.com>",
+//     }),
+//   ],
+// };
+// // export default NextAuth({
+// //   providers: [
+// //     LinkedInProvider({
+// //       clientId: process.env.LINKEDIN_CLIENT_ID,
+// //       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+// //     }),
+// //     GoogleProvider({
+// //       clientId: process.env.GOOGLE_ID,
+// //       clientSecret: process.env.GOOGLE_SECRET,
+// //     }),
+
+// //     EmailProvider({
+// //       server: process.env.MAIL_SERVER,
+// //       from: "NextAuth.js <no-reply@example.com>",
+// //     }),
+// //   ],
+// // });
