9d0fd4aa67323329dea0b605876795bc5e14018a
Rayson N Mendes <raysonmendes2.0@gmail.com>
2022-10-07 21:41:55 -0300

    Feat: Creat login page

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,22 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { StyledMenu, Text } from "./styles";
 const linkStyle = {
   marginRight: 15,
 };
 
-const Menu = () => (
-  <StyledMenu>
-    <Link href="/">
-      <Text>Home</Text>
-    </Link>
-    <Link href="/servicos">
-      <Text>Serviços</Text>
-    </Link>
-    <Link href="/sobre">
-      <Text>Sobre</Text>
-    </Link>
-  </StyledMenu>
-);
+const Menu = () => {
+  const [isActive, setActive] = useState(false);
+  const heandleActive = () => {
+    setActive(!isActive);
+  };
+  return (
+    <StyledMenu>
+      <Link href="/">
+        <Text>Home</Text>
+      </Link>
+      <Link href="/servicos">
+        <Text>Serviços</Text>
+      </Link>
+      <Link href="/sobre">
+        <Text>Sobre</Text>
+      </Link>
+    </StyledMenu>
+  );
+};
 
 export default Menu;
