17e7a46f076f3ee1f490d21d5201d97a954bc572
Marcos Amorim <markstm10@gmail.com>
2022-10-10 00:18:29 -0300

    feat: (sign user) creates user on db when log in

--- src/pages/api/auth/[...nextauth].jsx
+++ src/pages/api/auth/[...nextauth].jsx
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../lib/mongodb";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -13,5 +15,36 @@ export const authOptions = {
     encryption: true,
   },
   secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      const { email, name } = user;
+
+      try {
+        const { db } = await clientPromise();
+
+        const filter = { email: email };
+        const options = { upsert: true };
+
+        //cria objeto com informações do usuário
+        const updateDoc = {
+          $setOnInsert: {
+            name: name,
+            email: email,
+            cpf: "",
+            cel: "",
+            role: ["client"],
+          },
+        };
+
+        const result = await db
+          .collection("users")
+          .updateOne(filter, updateDoc, options);
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 export default NextAuth(authOptions);
