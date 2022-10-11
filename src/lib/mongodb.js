10091b1a7808d5eb16d4f5f8ffee61b00e76a8b5
Marcos Amorim <markstm10@gmail.com>
2022-10-11 00:45:36 -0300

    feat: (mongoose) install and sets mongoose to handle mongo db

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,67 +1,92 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
-const dbname = process.env.MONGODB_DB;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cachedDb;
-let cachedClient;
-
-if (!uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI env var");
 }
 
-if (!dbname) {
-  throw new Error("Please define the MONGODB_DB variable");
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export async function connectToDatabase() {
-  if (cachedDb && cachedClient) {
-    return { client: cachedClient, db: cachedDb };
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  if (!cached.promise) {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-  const db = await client.db(dbname);
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
 
-  cachedClient = client;
-  cachedDb = db;
-
-  return { client, db };
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectToDatabase;
 
+// import mongoose from "mongoose";
+
+// const connectToDatabase = (handler) => async (req, res) => {
+//   if (mongoose.connections[0].readyState) {
+//     // use current db connection
+//     return handler(req, res);
+//   }
+
+//   // use new db conncection
+//   await mongoose.connect(process.env.mongodburl, {
+//     useUnifiedTopology: true,
+//     // useFindAndModify: false,
+//     // useCreateIndex: true,
+//     useNewUrlParser: true,
+//   });
+
+//   return handler(req, res);
+// };
+
+// export default connectToDatabase;
+
 // import { MongoClient } from "mongodb";
 
 // const uri = process.env.MONGODB_URI;
+// const dbname = process.env.MONGODB_DB;
 
-// const options = {};
+// let cachedDb;
+// let cachedClient;
 
-// let client;
-// let clientPromise;
-
-// if (!process.env.MONGODB_URI) {
+// if (!uri) {
 //   throw new Error("Please add your Mongo URI to .env.local");
 // }
 
-// if (!process.env.NODE_ENV === "development") {
-//   // In development mode, use a global variable so that the value
-//   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     global._mongoClientPromise = client.connect();
-//   }
-
-//   clientPromise = global._mongoClientPromise;
-// } else {
-//   // In production mode, it's best to not use a global variable.
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect();
+// if (!dbname) {
+//   throw new Error("Please define the MONGODB_DB variable");
 // }
 
-// // Export a module-scoped MongoClient promise. By doing this in a
-// // separate module, the client can be shared across functions.
-// export default clientPromise;
+// export async function connectToDatabase() {
+//   if (cachedDb && cachedClient) {
+//     return { client: cachedClient, db: cachedDb };
+//   }
+
+//   const client = await MongoClient.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+
+//   const db = await client.db(dbname);
+
+//   cachedClient = client;
+//   cachedDb = db;
+
+//   return { client, db };
+// }
+
+// export default connectToDatabase;
