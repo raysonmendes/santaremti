8c74f889dc2a75249bc58c3df950a6ba03304065
Rayson N Mendes <raysonmendes2.0@gmail.com>
2022-11-22 13:09:15 -0300

    Service page styling and structuring

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -14,8 +14,8 @@ export const GlobalStyle = createGlobalStyle`
     *{
         margin: 0;
         padding: 0;
-
         box-sizing: border-box;
+        
     }
 
     body{
---
+++ src/styles/pg_Servicos/stl_Servicos.jsx
@@ -0,0 +1,11 @@
+import { Children } from "react";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
