dfb0a26fdc43989af34e22e8b7f971ac03ba15f6
Marcos Amorim <markstm10@gmail.com>
2022-11-23 02:29:36 -0300

    styles: (wrapper) adds props to wrapper component

--- src/components/wrapper/index.jsx
+++ src/components/wrapper/index.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 import { Container } from "./styles";
 
-function Wrapper({ children, style }) {
-  return <Container style={style}>{children}</Container>;
+function Wrapper({ children, style, center, row, column, padding }) {
+  return (
+    <Container
+      center={center}
+      style={style}
+      row={row}
+      column={column}
+      padding={padding}
+    >
+      {children}
+    </Container>
+  );
 }
 
 export default Wrapper;
