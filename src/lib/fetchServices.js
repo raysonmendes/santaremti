52ae880afd5046ddb4964e350625f2727e869017
Rayson N Mendes <raysonmendes2.0@gmail.com>
2022-11-25 09:10:09 -0300

    clean code

--- src/lib/fetchServices.js
+++ src/lib/fetchServices.js
@@ -1,17 +1,3 @@
-// import Service from "../models/service";
-// import connectToDatabase from "./mongodb";
-
-// //busca os serviços no banco de dados
-// const getServices = async () => {
-//   console.log("to executando get sertvices");
-//   await connectToDatabase();
-//   const response = await Service.find({});
-
-//   return response;
-// };
-
-// export default getServices;
-
 import Service from "../models/service";
 import connectToDatabase from "./mongodb";
 
