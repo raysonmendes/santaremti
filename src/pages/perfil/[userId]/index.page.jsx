19c0781c0de4f245c995f9dadad2590fe02ef3bd
Marcos Amorim <markstm10@gmail.com>
2022-12-16 00:53:20 -0300

    feat: (profile) adds profile info to the profile page

--- src/pages/perfil/[userId]/index.page.jsx
+++ src/pages/perfil/[userId]/index.page.jsx
@@ -1,36 +1,102 @@
+import { unstable_getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Wrapper from "../../../components/wrapper";
+import { getUserByEmail } from "../../../lib/fetchUsers";
+import { authOptions } from "../../api/auth/[...nextauth].page";
 // import { getUserByEmail } from "../../lib/fetchUsers";
 
-function ProfilePage() {
-  const [user, setUser] = useState(null);
+import Image from "next/image";
 
-  const session = useSession();
-  const router = useRouter();
-
-  const { userId } = router.query;
-
-  useEffect(() => {
-    // if (session) {
-    //   async function getUser() {
-    //     const usuario = await getUserByEmail(session.data.user.email);
-    //   }
-    //   getUser();
-    // }
-    console.log("router query>: ", userId);
-  }, []);
-
+function ProfilePage({ user }) {
+  console.log("user: ", user);
   return (
     <>
       <h2>Meu perfil</h2>
-      <Wrapper>
-        <strong>nome:</strong>
-        <p>{user?.name}</p>
+      <Wrapper padding>
+        <Image
+          src={user?.avatarUri}
+          alt="Avatar do usuário"
+          width={"90"}
+          height={"90"}
+          style={{ borderRadius: "50px" }}
+        />
+      </Wrapper>
+      <Wrapper flex column>
+        <Wrapper flex row padding="bottom">
+          <Wrapper padding="right">nome:</Wrapper>
+          <Wrapper>{user?.name}</Wrapper>
+        </Wrapper>
+        <Wrapper flex row padding="bottom">
+          <Wrapper padding="right">email:</Wrapper>
+          <Wrapper>{user?.email}</Wrapper>
+        </Wrapper>
+        <Wrapper flex row padding="bottom">
+          <Wrapper padding="right">tel:</Wrapper>
+          <Wrapper>{user?.tel}</Wrapper>
+        </Wrapper>
+        <Wrapper flex row padding="bottom">
+          <Wrapper padding="right">cpf:</Wrapper>
+          <Wrapper>{user?.cpf}</Wrapper>
+        </Wrapper>
+        <Wrapper flex row padding="bottom">
+          <Wrapper padding="right">Tipo de usuário:</Wrapper>
+          <Wrapper>{user?.role}</Wrapper>
+        </Wrapper>
       </Wrapper>
     </>
   );
 }
 
 export default ProfilePage;
+
+// busca dados do serviço específico
+export async function getServerSideProps(context) {
+  // busca o token do usuário logado
+  const token = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  // verifica se o user está logado, caso contrário, redireciona para o login
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/entrar",
+      },
+    };
+  }
+
+  let user;
+  // busca o usuário no banco de dados
+  try {
+    user = await getUserByEmail(token.user.email);
+    console.log("user encotnrado: ", user);
+  } catch (error) {
+    // redireciona em caso de erro na busca
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/404",
+      },
+    };
+  }
+
+  if (!user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/entrar",
+      },
+    };
+  }
+
+  // retorna as props para o componente
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user[0])),
+    },
+  };
+}
