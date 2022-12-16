dfb52c321393ce05fb6f3317bd92c78ad7eaac58
Marcos Amorim <markstm10@gmail.com>
2022-12-16 03:42:57 -0300

    feat: (service request) check if the user is a professional

--- src/pages/servicos/[serviceId]/index.page.jsx
+++ src/pages/servicos/[serviceId]/index.page.jsx
@@ -1,7 +1,6 @@
 import { unstable_getServerSession } from "next-auth";
 // import { getToken } from "next-auth/jwt";
 import React, { useState } from "react";
-import Modal from "react-modal";
 
 import Head from "../../../components/Head";
 import Wrapper from "../../../components/wrapper";
@@ -9,8 +8,27 @@ import CustomizedLink from "../../../components/customizedLink";
 import { getServiceById } from "../../../lib/fetchServices";
 import { authOptions } from "../../api/auth/[...nextauth].page";
 import { Section, FooterSection } from "./styles";
-import ModalOffer from "./components/offer-modal";
+import OfferForm from "./components/offer-modal";
 import { getUserByEmail } from "../../../lib/fetchUsers";
+import InputForm from "../criar/components/form-input";
+import FormTextArea from "../criar/components/form-text-area";
+import { SubmitBtn } from "./components/offer-modal/styles";
+
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#__next");
 
 function Servico({ service, user }) {
   const [isModalOfferOpen, setIsModalOfferOpen] = useState(false);
@@ -21,6 +39,11 @@ function Servico({ service, user }) {
 
   function handleSubmitOffer(event) {
     event.preventDefault();
+    if (user.role !== "professional") {
+      toggleModalOffer();
+      return;
+    }
+
     console.log("mandarei esses dados: ", event);
     const userId = user._id;
 
@@ -40,22 +63,74 @@ function Servico({ service, user }) {
         title="Serviços Abertos - Santarém TI"
         description="Página que exibe informações sobre o serviço"
       />
-      <Section>
-        <h1>Servico: {service.name}</h1>
-        <p>
-          <strong>Descrição</strong> : {service.description}
-        </p>
-        <FooterSection>
-          <button onClick={toggleModalOffer}>Fazer oferta</button>
-        </FooterSection>
-      </Section>
+      <Wrapper flex row>
+        <Section>
+          <Wrapper padding="left">
+            <h3>Faça sua proposta</h3>
+          </Wrapper>
+          <h1>Servico: {service.name}</h1>
+          <p>
+            <strong>Descrição</strong> : {service.description}
+          </p>
+        </Section>
 
-      <ModalOffer
-        isModalOfferOpen={isModalOfferOpen}
-        onSubmit={handleSubmitOffer}
-        onToggleModaOffer={toggleModalOffer}
-        service={service}
-      />
+        <Wrapper flex column>
+          <Wrapper padding="left">
+            <h3>Faça sua proposta</h3>
+          </Wrapper>
+
+          <Wrapper flex row>
+            <form
+              action="submit"
+              name="offer-form"
+              onSubmit={handleSubmitOffer}
+            >
+              <FormTextArea
+                formName={"offer-form"}
+                required={false}
+                textAreaId={"offer-description"}
+                textAreaLabel={"Descrição da Proposta"}
+                textAreaName={"offer-description"}
+              />
+              <InputForm
+                type={"number"}
+                inputId={"offered-price"}
+                required={true}
+                inputName={"offered-price"}
+                formName={"offer-form"}
+                inputLabel="Valor da propsta*"
+                inputPlaceholder="Digite o valor"
+              />
+
+              <Wrapper padding="left">
+                <SubmitBtn type="submit">Fazer proposta</SubmitBtn>
+              </Wrapper>
+            </form>
+          </Wrapper>
+        </Wrapper>
+      </Wrapper>
+
+      <Modal
+        isOpen={isModalOfferOpen}
+        style={customStyles}
+        contentLabel="Verification Modal"
+        onRequestClose={toggleModalOffer}
+      >
+        <Wrapper flex column>
+          <strong>
+            Você precisa ter uma conta de Profissional para poder fazer
+            propostas!
+          </strong>
+          <Wrapper flex row>
+            <Wrapper padding="top" gap="8px">
+              <div onClick={toggleModalOffer}>Cancelar</div>
+              <CustomizedLink link={`/perfil/${user._id}/edit`}>
+                <div>Editar Conta</div>
+              </CustomizedLink>
+            </Wrapper>
+          </Wrapper>
+        </Wrapper>
+      </Modal>
     </>
   );
 }
