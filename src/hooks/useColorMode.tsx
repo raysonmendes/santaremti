61e68cf0db31cd783c8d14e160fd8a055189a8f2
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-25 10:45:31 -0300

    refactor: (colorMode context hook) remoção de compontentes obsoletos

--- src/hooks/useColorMode.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import ColorModeContext from "../contexts/colorModeContext";
-
-const useColorMode = () => useContext(ColorModeContext);
-
-export default useColorMode;
