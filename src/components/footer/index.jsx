fc214c3c321d53edb46d3e0649cb3d3fcbd08f8c
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-26 12:33:44 -0300

    style:(footer) adequado para receber mudanças de tema dinamicamente

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import { Typography } from "@mui/material";
 import { Sct_Footer, Text, Container } from "../footer/style";
 
-import { useState } from "react";
 const Footer = () => {
-  const [isActive, setIsActive] = useState(false);
+  // const [isActive, setIsActive] = useState(false);
 
-  const handleIsActive = () => {
-    setIsActive(!isActive);
-  };
+  // const handleIsActive = () => {
+  //   setIsActive(!isActive);
+  // };
 
   return (
     <Container>
       <Sct_Footer>
-        <Text>COPYRIGHT © Todos os direitos reservados. 2022.</Text>
+        <Typography
+          variant="caption"
+          fontFamily={"Montserrat"}
+          fontSize={12}
+          fontStyle={"normal"}
+          lineHeight={"normal"}
+        >
+          COPYRIGHT © Todos os direitos reservados. 2022.
+        </Typography>
       </Sct_Footer>
     </Container>
   );
