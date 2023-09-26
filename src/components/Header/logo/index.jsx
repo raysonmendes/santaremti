a2b03e43b7ca1d839f4f8d865d3afed1ac8ee902
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-26 03:30:14 -0300

    style:(header logo) mudado a fonte da logo

--- src/components/Header/logo/index.jsx
+++ src/components/Header/logo/index.jsx
@@ -7,7 +7,7 @@ const Logo = () => (
   <Link href={"/"}>
     <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
       <Avatar src="https://res.cloudinary.com/rayson/image/upload/v1655049181/Santarem%20TI/FaveIcon/favicon.ico"></Avatar>
-      <Typography variant="h2" fontSize={24}>
+      <Typography variant="h2" fontSize={24} fontFamily={"Montserrat"}>
         Santarém TI
       </Typography>
     </Box>
