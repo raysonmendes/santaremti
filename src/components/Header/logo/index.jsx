d643fc77b1fc48b658b42e959634a04bfc3a0c8a
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-26 02:51:22 -0300

    style: (header logo) alterado a construção do logo

--- src/components/Header/logo/index.jsx
+++ src/components/Header/logo/index.jsx
@@ -1,21 +1,24 @@
-import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 import { StyledLogo, Text } from "./styles";
+import { Avatar, Box, Typography } from "@mui/material";
 
 const Logo = () => (
   <Link href={"/"}>
-    <StyledLogo>
-      <Image
-        src="https://res.cloudinary.com/rayson/image/upload/v1655049181/Santarem%20TI/FaveIcon/favicon.ico"
-        width="40px"
-        height="40px"
-        objectFit={"cover"}
-        layout="fixed"
-      />
-
-      <Text>Santarém TI</Text>
-    </StyledLogo>
+    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Avatar src="https://res.cloudinary.com/rayson/image/upload/v1655049181/Santarem%20TI/FaveIcon/favicon.ico">
+        {/* <Image
+          
+          width="40px"
+          height="40px"
+          objectFit={"cover"}
+          layout="fixed"
+        /> */}
+      </Avatar>
+      <Typography variant="h2" fontSize={24}>
+        Santarém TI
+      </Typography>
+    </Box>
   </Link>
 );
 
