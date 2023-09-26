0868f4c6cdbe075fe8d68624a7f05b0a3917c34d
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-26 15:20:47 -0300

    style: (home page) alterado os componentes de estilo para componentes do material ui

--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -14,6 +14,7 @@ import {
 } from "../styles/homePage";
 import Image from "next/image";
 import CustomizedLink from "../components/customizedLink";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -22,7 +23,61 @@ export default function Home() {
         title="Home - Santarém TI"
         description="Santarém TI, aqui você encontra os melhores profissionais de TI."
       />
-      <Section1>
+
+      <Box
+        maxWidth={"100%"}
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <Typography
+          variant="h3"
+          fontFamily={"Barlow"}
+          fontSize={64}
+          fontStyle={"normal"}
+          fontWeight={700}
+          lineHeight={"normal"}
+        >
+          Bem vindo a Santarém TI
+        </Typography>
+
+        <Box display={"flex"} width={"100%"} gap={1} alignItems={"center"}>
+          <Box display={"flex"} flexDirection={"column"} flexBasis={"50%"}>
+            <Typography
+              variant="body1"
+              fontFamily={"Manrope"}
+              fontSize={32}
+              fontStyle={"normal"}
+              fontWeight={400}
+              lineHeight={"normal"}
+            >
+              Encontre os melhores profissionais de TI da nossa região.
+            </Typography>
+            <Box display={"flex"}>
+              <Button variant="outlined">Prestar Serviço</Button>
+              <Button variant="contained"> Solicitar Serviço</Button>
+            </Box>
+          </Box>
+          <Box
+            display={"flex"}
+            flexBasis={"50%"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Image
+              src={
+                "https://res.cloudinary.com/rayson/image/upload/v1654613155/Santarem%20TI/Home%20Page/img_Home.png"
+              }
+              width={500}
+              height={500}
+              alt="Uma imagem com o desenho de uma pessoa que trabalha na area de T.I. refletindo sobre algo"
+              priority={true}
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      {/* <Section1>
         <Sct_Title>
           <Title1>Bem vindo à Santarém TI</Title1>
         </Sct_Title>
@@ -52,7 +107,7 @@ export default function Home() {
             />
           </Sct_Image>
         </Sct_Intro>
-      </Section1>
+      </Section1> */}
     </>
   );
 }
