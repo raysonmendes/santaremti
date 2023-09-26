393e888c1e5e57f05ae894b294d6da2681be58ff
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-09-26 03:31:50 -0300

    style: (header menu) mudado o tamanho da fonte dos itens de menu

--- src/components/Header/menu/styles.jsx
+++ src/components/Header/menu/styles.jsx
@@ -17,12 +17,10 @@ export const Text = styled.h1`
   font-family: "Montserrat", sans-serif;
   font-style: normal;
   font-weight: 400;
-  font-size: 1.6rem;
+  font-size: 1rem;
   line-height: 1.08rem;
   color: #040f0f;
-  //width: 65px;
   padding: 8.68px;
-  //margin: auto;
   text-align: center;
   &:hover {
     cursor: pointer;
